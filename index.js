51c71306570ddea4924f01e438d94974b5856a37
abhi793199 <abhi793199@gmail.com>
2024-01-17 18:27:47 +0530

    with arrow function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function vowel()
+
+    //with function keyword
+
+/*function vowel()
 {
     let c=0;
     let n=prompt("Enter any word i'll count how many vowels in it ");
@@ -12,4 +15,23 @@ function vowel()
     alert(`In ${n} numbers of vowel is ${c}`);
 }
 
-vowel();
+vowel();*/
+
+
+        //using arrow keyword
+
+        const vowel= ()=>
+        {
+            let c=0;
+            let n=prompt("Enter any word i'll count how many vowels in it ");
+            for (let i of n) {
+                if(i=='a'|| i=="e" || i=="i" || i=="o" ||i=="u")
+                {
+                    c++;
+                    console.log(`${i} is a vowel`);
+                }
+            }
+            alert(`In ${n} numbers of vowel is ${c}`);
+        }
+        
+        vowel();
